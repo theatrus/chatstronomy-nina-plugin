31810c469ee961bc2a232f8ea6b810fcf63d9f46
Yann Ramin <github@theatr.us>
2026-08-16 15:42:51 -0700

    Publish plugin 0.1.0.18 to NINA registry (#22)

--- build-package.ps1
+++ build-package.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 param(
     [ValidatePattern('^\d+\.\d+\.\d+\.\d+$')]
-    [string] $Version = '0.1.0.17',
+    [string] $Version = '0.1.0.18',
 
     [ValidateNotNullOrEmpty()]
     [string] $InstallerUrl = '',
