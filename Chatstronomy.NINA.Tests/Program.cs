5083811cd7757b3a111d4d1ab291e13cb497bcb0
Yann Ramin <github@theatr.us>
2026-08-16 19:20:10 -0700

    Use Chatstronomy website in plugin metadata

--- Chatstronomy.NINA.Tests/Program.cs
+++ Chatstronomy.NINA.Tests/Program.cs
@@ -1104,7 +1104,7 @@ internal static class Program
         4242,
         Guid.Parse("460a8c62-28ce-4781-92e5-ab2440982175"),
         "North Rig",
-        "0.1.0.18",
+        "0.1.0.19",
         "3.2.0.9001",
         new DirectCapabilities(true, true, true, true, true, true, true, true));
 
