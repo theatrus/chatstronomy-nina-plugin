9dcfd51dca61c890bb2d8a4ea326c37300fedc58
Yann Ramin <github@theatr.us>
2026-08-26 22:27:46 -0700

    Prepare Chatstronomy N.I.N.A. 0.1.0.24

--- build-package.ps1
+++ build-package.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 param(
     [ValidatePattern('^\d+\.\d+\.\d+\.\d+$')]
-    [string] $Version = '0.1.0.23',
+    [string] $Version = '0.1.0.24',
 
     [ValidateNotNullOrEmpty()]
     [string] $InstallerUrl = '',
