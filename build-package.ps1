9bb304e1e76ed3608fb711c1a4e3c8a038bca05b
Yann Ramin <github@theatr.us>
2026-09-08 23:52:47 -0700

    Prepare plugin 0.1.0.27 with Chatstronomy 0.4.7

--- build-package.ps1
+++ build-package.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 param(
     [ValidatePattern('^\d+\.\d+\.\d+\.\d+$')]
-    [string] $Version = '0.1.0.26',
+    [string] $Version = '0.1.0.27',
 
     [ValidateNotNullOrEmpty()]
     [string] $InstallerUrl = '',
